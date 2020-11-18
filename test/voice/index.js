3413406c24f0e0252ef5bf55ef977900027fab2b
flexrouting+ci <flexrouting+ci@twilio.com>
2020-11-18 19:29:45 +0000

    0.5.3

--- test/voice/index.js
+++
@@ -1,8 +0,0 @@
-require('./inbound/spec/Reservation');
-require('./inbound/spec/Supervisor');
-require('./outbound/spec/Reservation');
-require('./outbound/spec/Supervisor');
-require('./outbound/spec/Task');
-require('./outbound/spec/TaskTransferToQueue');
-require('./outbound/spec/TaskTransferToWorker');
-require('./outbound/spec/ExternalTransfer');
